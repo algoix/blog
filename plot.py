e5be11e01bc5be468afd2678f63215c2ff675461
Partha Sen <research@parthasen.net>
2017-06-12 13:08:15 +0530

    Create plot.py

---
+++ plot.py
@@ -0,0 +1,14 @@
+"""Plot High prices for IBM"""
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def test_run():
+    df = pd.read_csv("data/IBM.csv")
+    #Your code here
+    df.High.plot()
+    plt.show()  # must be called to show plots
+
+
+if __name__ == "__main__":
+    test_run()
