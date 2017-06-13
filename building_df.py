b22bf4e2431ac3598d9c8afee3f924d940e2297e
Partha Sen <research@parthasen.net>
2017-06-13 15:53:01 +0530

    Create building_df.py

---
+++ building_df.py
@@ -0,0 +1,37 @@
+"""Utility functions"""
+
+import os
+import pandas as pd
+
+def symbol_to_path(symbol, base_dir="data"):
+    """Return CSV file path given ticker symbol."""
+    return os.path.join(base_dir, "{}.csv".format(str(symbol)))
+
+
+def get_data(symbols, dates):
+    """Read stock data (adjusted close) for given symbols from CSV files."""
+    df = pd.DataFrame(index=dates)
+    if 'SPY' not in symbols:  # add SPY for reference, if absent
+        symbols.insert(0, 'SPY')
+
+    for symbol in symbols:
+        #Read and join data for each symbol
+        df.join(symbol,'inner')
+
+    return df
+
+
+def test_run():
+    # Define a date range
+    dates = pd.date_range('2010-01-22', '2010-01-26')
+
+    # Choose stock symbols to read
+    symbols = ['GOOG', 'IBM', 'GLD']
+    
+    # Get stock data
+    df = get_data(symbols, dates)
+    print df
+
+
+if __name__ == "__main__":
+    test_run()
